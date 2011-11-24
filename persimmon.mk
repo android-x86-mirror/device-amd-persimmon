5b0713eec9dfae27675b7193c66ba1841769fdca
Tom Stellard <thomas.stellard@amd.com>
2011-11-24 15:38:18 -0500

    Remove uneeded copy file

--- persimmon.mk
+++ persimmon.mk
@@ -1,7 +1,6 @@
 PRODUCT_PACKAGES := $(THIRD_PARTY_APPS)
 
 PRODUCT_COPY_FILES := \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     $(LOCAL_PATH)/asound.conf:system/etc/asound.conf \
     $(LOCAL_PATH)/init.sh:system/etc/init.sh
 
